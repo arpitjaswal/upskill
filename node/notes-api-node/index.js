b9cef1af57be49ef7cbc6971f5a20ca4f742cc38
Arpit Jaswal <iamarpitjaswal@gmail.com>
2025-11-11 03:03:38 +0530

    trying to write fucking apis at 3 am in the fucking morning when ai can do my job

---
+++ node/notes-api-node/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bcrypt from "bcrypt"
+
+
+const app = express();
+app.use(express.json());
+
+//i think how many times i want the hashing to be salted for making it more secure
+const saltRounds = 10;
+
+
+
+
+api.post('/register',(req,res)=>{
+    
+})
+
